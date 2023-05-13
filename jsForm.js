7b6ad1f248bb9961a588df31595a0d399653bb9d
ParsaShafagh <108547105+PA-M0@users.noreply.github.com>
2023-05-14 00:35:47 +0330

    Add files via upload

---
+++ jsForm.js
@@ -0,0 +1,18 @@
+const emailInfo = document.querySelector('#email')
+const passwordInfo = document.querySelector('#password')
+const submitBtn = document.querySelector('#submitBtn')
+console.log(emailInfo)
+
+const checkForm =() =>{
+
+}
+
+submitBtn.addEventListener('submit',checkForm)
+
+
+
+
+
+
+
+    
